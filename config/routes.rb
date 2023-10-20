72b8e0888a014d33c79f617bc343cba79dc2d1c5
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-20 23:56:37 +0900

    fix a bit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-  }
   resources :users, only: [:show]
 end
