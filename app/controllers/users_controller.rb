46545d49a84e098f80041509ee83af919ccd3295
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-22 23:09:55 +0900

    add kaminari, pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(30)
   end
 
   def show
