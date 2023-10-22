65b3eedb70e6039c98ec5f86ffecef4f93b9b027
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-22 22:19:51 +0900

    add users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
