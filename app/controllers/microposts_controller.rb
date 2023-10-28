7d80872185884c784b74afcf47a9f719ea77c1f8
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-28 21:29:56 +0900

    micropostの操作はログイン必須に

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,8 @@
 class MicropostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[create destroy]
+  def create
+  end
+
+  def destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   resources :users, only: %i[index show destroy]
+  resources :microposts, only: %i[create destroy]
 end
