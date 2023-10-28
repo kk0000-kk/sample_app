5d49f69d05a3f0ed092d33e6914b89d89b0e0800
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-28 22:52:29 +0900

    post form作成、ホーム表示更新

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,8 +1,21 @@
 class MicropostsController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = 'Micropost created!'
+      redirect_to root_url
+    else
+      render 'static_pages/home', status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
   end
 end
