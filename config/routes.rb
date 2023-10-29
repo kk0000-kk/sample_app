9d1bf3a8342654a23e1a5e256501beefd2bbee61
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-29 22:47:30 +0900

    フォロー/フォロワーの表示

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,12 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users, only: %i[index show destroy]
   resources :microposts, only: %i[create destroy]
+  resources :relationships, only: %i[create destroy]
 end
