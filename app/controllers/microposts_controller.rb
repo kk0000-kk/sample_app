4535c0b1e834576b5d5f7a1ebd46fa6a5cc7a145
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-29 09:25:43 +0900

    micropostの削除

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
+  before_action :correct_user, only: :destroy
+
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -12,11 +14,23 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash.now[:success] = 'Micropost deleted'
+    if request.referer.nil?
+      redirect_to root_url, status: :see_other
+    else
+      redirect_to request.referer, status: :see_other
+    end
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url, status: :see_other if @micropost.nil?
   end
 end
