40558fc98639fbe38d47ad9881eb698a43e39fed
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-29 09:05:04 +0900

    フィード表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,10 @@
 
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if user_signed_in?
+    if user_signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.page(params[:page])
+    end
   end
 
   def help; end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :microposts, dependent: :destroy
+
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where(user_id: id)
+  end
 end
