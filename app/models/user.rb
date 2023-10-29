93e47faee5cf5c7c293178463e7168583c1158a5
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-29 21:53:07 +0900

    リレーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :confirmable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
 
   # 試作feedの定義
   # 完全な実装は次章の「ユーザーをフォローする」を参照
