62874334ca076b3f165f4a89c4189ac2fc958473
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-29 09:55:26 +0900

    画像Upのバリデーション

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,7 @@ class Micropost < ApplicationRecord
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 140 }
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png],
+                                    message: 'must be a valid image format' },
+                    size: { less_than: 5.megabytes, message: 'should be less than 5MB' }
 end
