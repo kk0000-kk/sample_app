ad7d70f61b089e3b4a43abe4c2011df38881b8e2
K.Kobayashi <kobayashi@sonicgarden.jp>
2023-10-29 23:02:16 +0900

    フォロー/フォロワーの一覧表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: %i[index destroy]
+  before_action :authenticate_user!, only: %i[index destroy following followers]
   before_action :admin_user, only: :destroy
   before_action :set_user, only: %i[show destroy]
 
@@ -14,6 +14,20 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy!
     redirect_to users_url, status: :see_other, notice: 'User deleted'
+  end
+
+  def following
+    @title = 'Following'
+    @user  = User.find(params[:id])
+    @users = @user.following.page(params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'Followers'
+    @user  = User.find(params[:id])
+    @users = @user.followers.page(params[:page])
+    render 'show_follow'
   end
 
   private
